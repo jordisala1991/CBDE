541c8ec22ef618c5186c181fab523bdff391a989
Jordi Sala <jordism91@gmail.com>
2013-05-03 23:54:23 +0200

    Connection is now attribute of the class

--- src/cbde/db/OracleFirstScript.java
+++ src/cbde/db/OracleFirstScript.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class OracleFirstScript {
+	
+	private Connection connection;
 
 	private static final String URL = "jdbc:oracle:thin:@oraclefib.fib.upc.es:1521:ORABD";
 	private static final String USERNAME = "daniel.llamazares";
@@ -12,7 +14,7 @@ public class OracleFirstScript {
 	
 	public OracleFirstScript() throws SQLException {
 	
-		Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 	}
 	
 }
