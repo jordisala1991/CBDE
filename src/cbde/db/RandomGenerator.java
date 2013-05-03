a868b7a7e8707976a826edff76bac2c56409482a
Jordi Sala <jordism91@gmail.com>
2013-05-03 22:14:20 +0200

    Added generation of date properly, and fixed error in random generation of integers between 2 numbers INCLUSIVE

--- src/cbde/db/RandomGenerator.java
+++ src/cbde/db/RandomGenerator.java
@@ -1,5 +1,8 @@
 package cbde.db;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Random;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -7,9 +10,11 @@ import org.apache.commons.lang3.RandomStringUtils;
 public class RandomGenerator {
 	
 	private Random random;
+	private GregorianCalendar calendar;
 	
 	public RandomGenerator() {
 		random = new Random();
+		calendar = new GregorianCalendar(0, 0, 0);
 	}
 	
 	private static int pow(int base, int exp) {
@@ -24,12 +29,12 @@ public class RandomGenerator {
 	}
 	
 	public int randomInt(int min, int max) {
-		return random.nextInt(max - min)+min;
+		return random.nextInt(max - min + 1)+min;
 	}
 	
 	public int randomInt(int numDigits) {
-		int min = pow(10, numDigits -1);
-		int max = min*10 -1;
+		int min = pow(10, numDigits - 1);
+		int max = min*10 - 1;
 		return randomInt(min, max);
 	}
 	
@@ -37,11 +42,14 @@ public class RandomGenerator {
 		return RandomStringUtils.randomAlphanumeric(numChars);
 	}
 	
-	public String randomDate() {
-		int dia = randomInt(1, 28);
-		int mes = randomInt(1, 12);
-		int any = randomInt(1800, 2013);
-		return dia + "/" + mes + "/" + any;
+	public Date randomDate() {
+
+		calendar.set(Calendar.YEAR, randomInt(1901, 2013));
+		calendar.set(Calendar.MONTH, randomInt(0, 11));
+		int maxDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+		calendar.set(Calendar.DAY_OF_MONTH, randomInt(1, maxDay));
+		
+		return calendar.getTime();
 	}
 
 	public Object getRandomItem(List<?> partSupp) {
